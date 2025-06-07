8272966b958e182d3e7100cfab674dac0ebf3c86
Lizzie Davison <lizzie@hubblehq.com>
2025-06-08 00:29:02 +0100

    feat: Move styles into the app modules styles
    Honestly once everything else was done it kinda glowed that the main
    stuff hadn't been moved out. So huzzah! It is done.
    
    NEXT TO THEMEING.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Timer } from './components/Timer'
 import { NextPrize } from "./components/NextPrize";
 import { Stickers } from "./components/Stickers";
 
+import styles from './App.module.css';
+
 export function App() {
   const { state } = useCountdown();
 
   return (
-    <main data-state={state()}>
+    <main className={styles['App']} data-state={state()}>
       <Timer />
       <NextPrize />
       <Stickers />
