8e85b85a553d7320d55a69ebd60f40a4c12cd3ec
Lizzie Davison <lizzie@hubblehq.com>
2025-06-08 13:08:30 +0100

    feat: Add plant theme
    So I haven't added any kind of theme changer or anything and really its
    not a theme yet I just wanted to work out what might look good. So I
    added a background, changed the next prize icons and the colors.
    
    Next to start actual themeing!

--- src/components/NextPrize/NextPrize.jsx
+++ src/components/NextPrize/NextPrize.jsx
@@ -3,9 +3,9 @@ import { createSignal } from "solid-js";
 import { useCountdown } from "../../context/Countdown";
 import styles from './NextPrize.module.css'
 
-const imgPrizeUrl = "https://cdn-icons-png.flaticon.com/512/9036/9036098.png";
+const imgPrizeUrl = "https://cdn-icons-png.flaticon.com/512/9470/9470902.png";
 const imgPrizeUrlCompleted =
-  "https://cdn-icons-png.flaticon.com/512/6039/6039671.png";
+  "https://cdn-icons-png.flaticon.com/512/11191/11191732.png";
 
 export function NextPrize() {
   const { state } = useCountdown();
@@ -15,7 +15,7 @@ export function NextPrize() {
     const storedPrize = localStorage.getItem("nextPrize")
     return storedPrize ? storedPrize : "";
   }
-  
+
   function updateNextPrize(nextPrize) {
     setNextPrize(nextPrize);
     localStorage.setItem("nextPrize", nextPrize)
